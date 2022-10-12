4fb327d34e2b93e94e89410816c60dcf9949fc27
rubis18 <114744926+rubis18@users.noreply.github.com>
2022-10-12 18:37:04 -0500

    Se agrega Act1 y act2

---
+++ P2ACT1SB1/hello.php
@@ -0,0 +1,28 @@
+<!-- <link rel="stylesheet" href="miestilo.css" type="text/css"> 
+El link no coincidia y opte por poner el estilo en el mismo codigo-->
+<title>Actividad 1</title>
+<style type="text/css">
+            div{
+            font-family: Broadway;
+            font-size: 40px;
+            text-align: center;
+            color: #8E2323;
+            background-color:#B1C6C6;
+            width: 50%;
+        }
+        body{
+            background-image: url(https://i.pinimg.com/736x/6e/da/0d/6eda0d66d1cce8e5baf64f1d8347cf7e.jpg);
+        }
+</style>
+<body><center><br><br><br><br><br><br><br><div>
+    <!-- Este codigo nos muestra lineas que podemos usar para mostrar mensajes
+Quevedo Mancilla Gloria Yazmin -->
+
+ <?php
+  echo 'Hola a todos'; 
+  // print("Hola a todos"); 
+  print("<br>Bienvenidos al mundo PHP");
+  print("<br>Soy Quevedo Mancilla Gloria Yazmin<br>");
+  printf("Del grupo de Programaciòn de 5PV");
+ ?>
+</div></center></body>
---
+++ P2ACT2SB1/hoy.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Actividad 2</title>
+	<style type="text/css">
+		div{
+            font-family: Bradley Hand ITC;
+            font-size: 50px;
+            text-align: center;
+            background-color: #E5D5BA ;
+            width: 70%;
+        }
+        body{
+            background-image: url(https://wallpapercave.com/uwp/uwp1563984.png);
+        }
+
+	</style>
+</head>
+<body>
+<!-- Este codigo nos muestra la fecha exacta de cuando lo ejecutamos
+Quevedo Mancilla Gloria Yazmin -->
+<center><br><br><br><br><div>
+	<?php
+		print("Bienvenidos a PHP");
+		printf("<br>Quevedo Mancilla Gloria Yazmin");
+		echo("<br>Progrmaciòn 5PV");
+		print("<br>Desarrolla Aplicaciones Web");
+		print("<br><br>La fecha del dia de hoy:<br>");
+		echo date('d-m-y');
+
+	?>
+</div></center>
+</body>
+</html>
